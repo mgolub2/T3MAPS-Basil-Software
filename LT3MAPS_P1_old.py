d0a06a87688f11c4d5c3d186712350ad35cd44cb
musicalrunner <therunner05@gmail.com>
2014-06-06 10:46:19 -0700

    Added module for specific implementation of LT3maps chip.

--- LT3MAPS_P1_old.py
+++ LT3MAPS_P1_old.py
@@ -13,8 +13,26 @@ class ChipWithInstruments(Chip):
     """
 
     def __init__(self):
+        """
+        Connect to the function and data generators and to the counter.
+
+        """
         self.function_generator = visa.GpibInstrument("GPIB::5",board_number=0)
         self.data_generator = visa.GpibInstrument("GPIB::3", board_number=0)
         self.counter = visa.GpibInstrument("GPIB::10", board_number=0)
-        #self.function_generator.write("VOLT 0.2")
-        self.data_generator.ask("DISP?")
+
+    def test_connections(self):
+        # Sets the voltage on the function generator to 0.2V
+        self.function_generator.write("VOLT 0.2")
+        
+        # Print the display of the data generator
+        print self.data_generator.ask("DISP?")
+        
+        # Presets the counter to default and selects the positive trigger slope
+        self.counter.write("*RST");
+        self.counter.write(":CONF:TOT:CONT")
+
+if __name__ == "__main__":
+    myChip = ChipWithInstruments()
+    print "testing connections"
+    myChip.test_connections()
