46b1670238ac61d245be6a84d124356d9d714eb8
Sam Kohn <kohn@berkeley.edu>
2014-07-02 17:47:45 -0700

    Skeleton file of test injection scan.

---
+++ scan_inject.py
@@ -0,0 +1,15 @@
+"""
+Inject charge onto the chip and see if we can see it.
+
+"""
+from new_setup.lt3maps import *
+
+chip = Pixel("new_setup/lt3maps.yaml")
+
+# set up configuration register
+
+# set up injection
+
+# run
+
+# output
