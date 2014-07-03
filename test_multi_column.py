a9e2bd72c9b4cf095f4506abb169ad72f8abdf97
Sam Kohn <kohn@berkeley.edu>
2014-07-03 11:14:29 -0700

    Add command-line arguments for specifying which columns to test. Add unique pattern for each column.

--- test_multi_column.py
+++ test_multi_column.py
@@ -2,33 +2,41 @@
 Test the ability of the lt3maps software to read and write to multiple columns.
 
 """
+import argparse
 from new_setup.lt3maps import *
 
-# initialize the chip
-chip = Pixel("new_setup/lt3maps.yaml")
 
-# Read from 2 different columns
-chip.set_global_register(column_address=1)
+def test_columns(min_col, max_col):
+    # initialize the chip
+    chip = Pixel("new_setup/lt3maps.yaml")
 
-chip.write_global_reg()
+    for i in range(min_col,max_col+1):
+        chip.set_global_register(column_address=i)
 
-chip.set_pixel_register('1000'*16)
+        chip.write_global_reg()
 
-chip.write_pixel_reg()
+        chip.set_pixel_register('1' + '0'*i + '1' + '0' * (64-i-2))
 
-chip.set_global_register(column_address=10)
+        chip.write_pixel_reg()
 
-chip.write_global_reg()
+        chip.set_pixel_register('1000'*16)
 
-print chip._blocks
+        chip.write_pixel_reg()
 
-chip.set_pixel_register('10'*32)
+    chip.run_seq()
 
-chip.write_pixel_reg()
+    output = chip.get_sr_output(invert=True)
+
+    for i in range(min_col, max_col + 1):
+        j = i-min_col
+        column_output = output[(128*j):(128*(j+1))]
+        print "column", i
+        print column_output
 
 
-chip.run_seq()
-
-output = chip.get_sr_output(invert=True)
-
-print output
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("min_col", help="the low column number to test", type=int)
+    parser.add_argument("max_col", help="the high column number to test", type=int)
+    args = parser.parse_args()
+    test_columns(args.min_col, args.max_col)
