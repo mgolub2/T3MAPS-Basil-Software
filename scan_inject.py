796ccc0fd9b73cec4675ac7b059c84d6d9542707
Sam Kohn <kohn@berkeley.edu>
2014-07-08 09:39:34 -0700

    Add global dac configuration and more stb/ctrl configurations.

--- scan_inject.py
+++ scan_inject.py
@@ -5,43 +5,60 @@ Inject charge onto the chip and see if we can see it.
 from new_setup.lt3maps import *
 
 chip = Pixel("new_setup/lt3maps.yaml")
+column_number = 0
 
-# enable injection on a particular pixel (column 0, pixel 5, for example)
-
-# select column 0
-chip.set_global_register(column_address=0)
-chip.write_global_reg()
-
-# mark pixel 64 (in this case) to be enabled
-chip.set_pixel_register('0'*63+ '1')
-chip.write_pixel_reg()
-
-# load the "enable" configuration
+# set up the global dac register
+"""
 chip.set_global_register(
-        column_address = 0,
-        #S0=1,
-        #HITLD_IN=1,
-        LD_IN0_7=bitarray('00000000'),
-        LDENABLE_SEL=0,
-        PrmpVbp=24,
-        PrmpVbf=2,
+        PrmpVbp=142,
+        PrmpVbf=11,
         vth=255,
         DisVbn=255,
         VbpThStep=38,
         PrmpVbnFol=68
         )
-
+"""
+chip.set_global_register(
+        PrmpVbp=142,
+        PrmpVbf=11,
+        vth=150,
+        DisVbn=49,
+        VbpThStep=100,
+        PrmpVbnFol=35,
+        column_address=63
+        )
 chip.write_global_reg(load_DAC=True)
+
+# enable injection on a particular pixel (column 0, pixel 5, for example)
+
+# select column 0
+chip.set_global_register(column_address=column_number)
+chip.write_global_reg()
+
+# mark pixel 64 (in this case) to be enabled
+chip.set_pixel_register('0'*63 + '1')
+chip.write_pixel_reg()
+
+# load the "enable" configuration by letting the
+# latches become transparent
+chip.set_global_register(
+        column_address=column_number,
+        LD_IN0_7=bitarray('00000110'),
+        LDENABLE_SEL=1,
+        )
+chip.write_global_reg()
+
+# un-transparentify the latches
+chip.set_global_register(
+        column_address=column_number,
+        #S0=1,
+        #HITLD_IN=1
+        )
+chip.write_global_reg()
 
 print chip._blocks
 
 # remove the "1" from the pixel shift register
-
-# first select the column
-chip.set_global_register(column_address = 0)
-chip.write_global_reg()
-
-# then remove the 1
 chip.set_pixel_register('0'*64)
 chip.write_pixel_reg()
 
@@ -60,7 +77,6 @@ time.sleep(2)
 chip.reset_seq()
 
 # set up injection pulse
-chip.write_injection(400)
 chip.write_injection(400)
 chip.write_injection(400)
 chip.write_injection(400)
