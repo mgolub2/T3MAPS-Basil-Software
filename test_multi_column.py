b0ae5899905dafae7f4c265d56245f9edd28f89b
Sam Kohn <kohn@berkeley.edu>
2014-07-08 16:46:44 -0700

    Add "verbose" option to test_multi_column.py.

--- test_multi_column.py
+++ test_multi_column.py
@@ -6,7 +6,7 @@ import argparse
 from new_setup.lt3maps import *
 
 
-def test_columns(min_col, max_col):
+def test_columns(min_col, max_col, verbose):
     # initialize the chip
     chip = Pixel("new_setup/lt3maps.yaml")
 
@@ -30,13 +30,17 @@ def test_columns(min_col, max_col):
     for i in range(min_col, max_col + 1):
         j = i-min_col
         column_output = output[(128*j):(128*(j+1))]
-        print "column", i
-        print column_output
+        if verbose:
+            print "column", i
+            print column_output
+
+    print "result:", all(results)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("min_col", help="the low column number to test", type=int)
     parser.add_argument("max_col", help="the high column number to test", type=int)
+    parser.add_argument("-v", "--verbose", help="print SR output", action="store_true")
     args = parser.parse_args()
-    test_columns(args.min_col, args.max_col)
+    test_columns(args.min_col, args.max_col, args.verbose)
