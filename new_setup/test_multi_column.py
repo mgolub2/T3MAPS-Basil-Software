84be61a3d275db71f76f7ff212ea3f3bfdc6c980
Sam Kohn <kohn@berkeley.edu>
2014-07-10 15:26:00 -0700

    Change test_multi_column.py into a unittest.

--- new_setup/test_multi_column.py
+++ new_setup/test_multi_column.py
@@ -2,48 +2,42 @@
 Test the ability of the lt3maps software to read and write to multiple columns.
 
 """
-import argparse
+import unittest
 from lt3maps.lt3maps import *
 
+class TestColumns(unittest.TestCase):
+    def setUp(self):
+        # initialize the chip
+        self.chip = Pixel("lt3maps/lt3maps.yaml")
 
-def test_columns(min_col, max_col, verbose):
-    # initialize the chip
-    chip = Pixel("lt3maps/lt3maps.yaml")
+    def test_columns(self):
+        chip = self.chip
+        for i in range(16):
+            chip.set_global_register(column_address=i)
 
-    for i in range(min_col,max_col+1):
-        chip.set_global_register(column_address=i)
+            chip.write_global_reg()
 
-        chip.write_global_reg()
+            chip.set_pixel_register('1' + '0'*i + '1' + '0' * (64-i-2))
 
-        chip.set_pixel_register('1' + '0'*i + '1' + '0' * (64-i-2))
+            chip.write_pixel_reg()
 
-        chip.write_pixel_reg()
+            chip.set_pixel_register('1'*(i+1) + '0'*(16-(i+1)) + '0'*48)
 
-        chip.set_pixel_register('1'*(i+1) + '0'*(16-(i+1)) + '0'*48)
+            chip.write_pixel_reg()
 
-        chip.write_pixel_reg()
+        chip.run_seq()
 
-    chip.run_seq()
+        output = chip.get_sr_output(invert=True)
 
-    output = chip.get_sr_output(invert=True)
-
-    results = []
-    for i in range(min_col, max_col + 1):
-        j = i-min_col
-        column_output = output[(128*j):(128*(j+1))]
-        desired_output = [0]*64 + [1] + [0]*i + [1] + [0]*(64-i-2)
-        results.append(all(column_output == desired_output))
-        if verbose:
+        for i in range(16):
             print "column", i
+            column_output = output[(128*i):(128*(i+1))]
             print column_output
-
-    print "result:", all(results)
+            desired_output = [0]*64 + [1] + [0]*i + [1] + [0]*(64-i-2)
+            self.assertTrue(all(column_output[64:] == desired_output[64:]))
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("min_col", help="the low column number to test", type=int)
-    parser.add_argument("max_col", help="the high column number to test", type=int)
-    parser.add_argument("-v", "--verbose", help="print SR output", action="store_true")
-    args = parser.parse_args()
-    test_columns(args.min_col, args.max_col, args.verbose)
+    # Run the test
+    # 'buffer = True' causes prints to only go through if test fails.
+    unittest.main(buffer=True)
