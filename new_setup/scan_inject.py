2aac7f3cf2c9a957f95040f1b875626f1a0ae568
Sam Kohn <kohn@berkeley.edu>
2014-07-15 10:15:04 -0700

    Correct configurations for injection.
    Learned more about latches. Steps:
    - Send appropriate values into shift register.
    - Open latch with LDENABLE_SEL and LD_IN0_7 bits high
    - Close latch same way but with bits low

--- new_setup/scan_inject.py
+++ new_setup/scan_inject.py
@@ -1,6 +1,9 @@
 """
 Inject charge onto the chip and see if we can see it.
 
+Injection is done using an external device, such as a function
+generator.
+
 """
 from lt3maps.lt3maps import *
 import argparse
@@ -8,6 +11,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("column_number", type=int)
 args = parser.parse_args()
+column_number = args.column_number
 
 chip = Pixel("lt3maps/lt3maps.yaml")
 
@@ -23,69 +27,27 @@ chip.set_global_register(
         )
 chip.write_global_reg(load_DAC=True)
 
-# enable injection on a particular pixel
-
-# select column 0
-column_number = args.column_number
-chip.set_global_register(column_address=column_number)
-chip.write_global_reg()
-
-# mark pixel 64 (in this case) to be enabled
-chip.set_pixel_register('0'*63 + '1')
-chip.write_pixel_reg()
-
-# load the "enable" configuration by letting the
-# latches become transparent
 chip.set_global_register(
         column_address=column_number,
-        LD_IN0_7=bitarray('01100000'),
+        config_mode=3)
+chip.write_global_reg()
+
+chip.set_pixel_register("0"*64)
+chip.write_pixel_reg()
+
+chip.set_global_register(
+        column_address=column_number,
+        LD_IN0_7=bitarray('11111111'),
         LDENABLE_SEL=1,
         )
 chip.write_global_reg()
 
-# un-transparentify the latches
 chip.set_global_register(
         column_address=column_number,
-        #S0=1,
-        #HITLD_IN=1
+        LD_IN0_7=bitarray('00000000'),
+        LDENABLE_SEL=1,
         )
 chip.write_global_reg()
 
-# remove the "1" from the pixel shift register
-#chip.set_pixel_register('0'*64)
-#chip.write_pixel_reg()
-
 # run
 chip.run_seq()
-
-"""
-# output
-output = chip.get_sr_output(invert=True)
-for i in range(2):
-    set_output = output[(64*i):(64*(i+1))]
-    print "output", i
-    print set_output
-
-time.sleep(0.02)
-chip.reset_seq()
-
-# set up injection pulse
-for i in range(3):
-    chip.write_injection(400)
-
-chip.set_global_register(column_address=column_number)
-chip.write_global_reg()
-
-# get the output
-chip.set_pixel_register('0' * 64)
-chip.write_pixel_reg()
-
-chip.run_seq()
-
-# output
-output = chip.get_sr_output(invert=True)
-for i in range(2,3):
-    set_output = output[(64*(i-2)):(64*(i-1))]
-    print "output", i
-    print set_output
-"""
